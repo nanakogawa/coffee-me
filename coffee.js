3c7f7115f6070013161659932c31030f8482758c
nana kogawa <kogawanana@gmail.com>
2015-08-19 15:20:14 -0700

    added work in progress

--- coffee.js
+++ coffee.js
@@ -1,14 +1,14 @@
 $('.carousel').carousel({
-  interval: 3000
+  interval: 4500
 });
 
 $('#top-nav').affix({
 });
 
-/* $('#top-nav').on( 'affixed.bs.affix', function () {
-  $('.navbar-brand').toggleClass('in');
-}); */
-
 $('#top-nav').on( 'affixed.bs.affix', function () {
+  $('.navbar-brand').toggleClass('in');
+});
+
+$('#top-nav').on( 'affix-top.bs.affix', function () {
   $('.navbar-brand').toggleClass('in');
 });
